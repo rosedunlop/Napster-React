d28ea6fd34e02590ec694db161bc8a107817a368
rosedunlop <rosedunlop@outlook.com>
2021-11-01 19:01:47 +0000

    submit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
         <>
         <div className='LinkedIn'>
         <h4>Follow our LinkedIn!</h4>
-        <p><a>Rose</a></p>
-        <p><a>Reisli</a></p>
+        <p><a href="https://www.linkedin.com/in/rose-dunlop-b0683b21a">Rose</a></p>
+        <p><a href="https://www.linkedin.com/in/reisli-hysa-a3b5961ab/">Reisli</a></p>
         <p>&copy; Rose & Reisli Music Project</p>
         </div>
         </>
